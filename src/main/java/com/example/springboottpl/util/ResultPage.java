c9611432da9158e30a7fe59e457490a6c055a61b
koobe <koobe.liu@uaf.com.cn>
2023-02-23 10:31:57 +0800

    添加分页返回分装

---
+++ src/main/java/com/example/springboottpl/util/ResultPage.java
@@ -0,0 +1,21 @@
+package com.example.springboottpl.util;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ResultPage<T> implements Serializable {
+
+	private List<T> list;
+	private Integer pageNum;
+	private Integer pageSize;
+	private long total;
+
+}
